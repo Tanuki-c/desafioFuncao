0b4ee99636dae412530d8b60609936478ba2c559
Tanuki-c <thomazcuzziol@gmail.com>
2024-07-29 22:32:10 -0300

    meu primeiro commit

---
+++ dio.me/desafio.dio.js
@@ -0,0 +1,44 @@
+let nomeDoHeroi = "Babalu"
+let exp = 10001
+
+if (exp < 1000) {
+
+    console.log(" nivel Ferro")
+    console.log("O aventureiro de nome " + nomeDoHeroi, "esta no nivel Ferro, com " + exp, "de exp")
+}
+else if (exp >= 1001 && exp <= 2000) {
+
+    console.log(" nivel Bronze")
+    console.log("O aventureiro de nome " + nomeDoHeroi, "esta no nivel Bronze, com " + exp, "de exp")
+}
+else if (exp >= 2001 && exp <= 5000) {
+
+
+    console.log(" nivel Prata")
+    console.log("O aventureiro de nome " + nomeDoHeroi, "esta no nivel Prata, com " + exp, "de exp")
+}
+else if (exp >= 5001 && exp <= 7000) {
+
+    console.log(" nivel Ouro")
+    console.log("O aventureiro de nome " + nomeDoHeroi, "esta no nivel Ouro, com " + exp, "de exp")
+}
+else if (exp >= 7001 && exp <= 8000) {
+
+    console.log(" nivel Platina")
+    console.log("O aventureiro de nome " + nomeDoHeroi, "esta no nivel Platina, com " + exp, "de exp")
+}
+else if (exp >= 8001 && exp <= 9000) {
+
+    console.log(" nivel Ascendente")
+    console.log("O aventureiro de nome " + nomeDoHeroi, "esta no nivel Ascendente, com " + exp, "de exp")
+}
+else if (exp >= 9001 && exp <= 10000) {
+
+    console.log(" nivel Imortal")
+    console.log("O aventureiro de nome " + nomeDoHeroi, "esta no nivel Imortal, com " + exp, "de exp")
+}
+else if (exp >= 10001) {
+
+    console.log(" nivel Radiante")
+    console.log("O aventureiro de nome " + nomeDoHeroi, "esta no nivel radiante, com " + exp, "de exp"  )
+}
